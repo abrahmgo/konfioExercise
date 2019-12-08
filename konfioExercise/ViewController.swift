b681266be7427002eadadcb341c247605a44fe43
Andres Bonilla <abonilla@miflink.com>
2019-12-07 19:31:36 -0600

    initial commit

---
+++ konfioExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  konfioExercise
+//
+//  Created by Andrés Abraham Bonilla Gómez on 12/7/19.
+//  Copyright © 2019 Andrés Abraham Bonilla Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ konfioExerciseTests/konfioExerciseTests.swift
@@ -0,0 +1,34 @@
+//
+//  konfioExerciseTests.swift
+//  konfioExerciseTests
+//
+//  Created by Andrés Abraham Bonilla Gómez on 12/7/19.
+//  Copyright © 2019 Andrés Abraham Bonilla Gómez. All rights reserved.
+//
+
+import XCTest
+@testable import konfioExercise
+
+class konfioExerciseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
